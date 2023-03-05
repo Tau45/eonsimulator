bad5a4a56b638763b403d5518015219dd2f0be77
Tau45 <tau45x@gmail.com>
2023-03-05 21:27:50 +0100

    Rejected connection not to be included in statistics again

--- src/event/EventNewCallArrivalErlangClass.cpp
+++ src/event/EventNewCallArrivalErlangClass.cpp
@@ -5,12 +5,11 @@ EventNewCallArrivalErlangClass::EventNewCallArrivalErlangClass(double occurrence
 		trafficClassStatistics(trafficClassStatistics) {}
 
 void EventNewCallArrivalErlangClass::execute(Network &network, priority_queue<Event *, vector<Event *>, Event::EventComparator> &eventQueue, Generator &generator) {
-	trafficClassStatistics.callsGenerated++;
-
 	Logger::instance().log(occurrenceTime, generator.getA(), generator.getSimulationIndex(), Logger::CONNECTION_SETUP, "Setting up connection between input: " + to_string(network.inputLinks[connection.getSourceLinkIndex()]->getSourceNode()) + " and output " + to_string(network.outputLinks[connection.getDestinationLinkIndex()]->getDestinationNode()) + "...");
 
 	switch (network.checkIfConnectionCanBeEstablished(connection, generator)) {
 		case Network::CONNECTION_CAN_BE_ESTABLISHED:
+			trafficClassStatistics.callsGenerated++;
 			connection.reserveResources();
 			eventQueue.push(new EventCallServiceTermination(occurrenceTime + connection.getServiceTime(), connection));
 
@@ -26,10 +25,12 @@ void EventNewCallArrivalErlangClass::execute(Network &network, priority_queue<Ev
 			Logger::instance().log(occurrenceTime, generator.getA(), generator.getSimulationIndex(), Logger::CONNECTION_REJECTED, "Connection rejected: free FSUs not found in source link (" + to_string(connection.getRequiredNumberOfFSUs()) + " FSUs)");
 			break;
 		case Network::INTERNAL_BLOCK:
+			trafficClassStatistics.callsGenerated++;
 			trafficClassStatistics.internalBlocksCount++;
 			Logger::instance().log(occurrenceTime, generator.getA(), generator.getSimulationIndex(), Logger::INTERNAL_BLOCK, "Connection rejected: internal blocking (" + to_string(connection.getRequiredNumberOfFSUs()) + " FSUs)");
 			break;
 		case Network::EXTERNAL_BLOCK:
+			trafficClassStatistics.callsGenerated++;
 			trafficClassStatistics.externalBlocksCount++;
 			Logger::instance().log(occurrenceTime, generator.getA(), generator.getSimulationIndex(), Logger::EXTERNAL_BLOCK, "Connection rejected: external blocking (" + to_string(connection.getRequiredNumberOfFSUs()) + " FSUs)");
 			break;
