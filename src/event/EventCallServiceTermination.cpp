c45622672a71cd3c8b5f53ff15e4c8338b331a10
Tau45 <tau45x@gmail.com>
2023-04-11 19:37:02 +0200

    memory management fix

--- src/event/EventCallServiceTermination.cpp
+++ src/event/EventCallServiceTermination.cpp
@@ -13,4 +13,5 @@ void EventCallServiceTermination::execute(Network &network, priority_queue<Event
 	} else {
 		Logger::instance().log(occurrenceTime, generator.getA(), generator.getSimulationIndex(), Logger::CONNECTION_CLOSED, "Connection between input: " + to_string(connection->getSourceLink()->getSourceNode()) + " and output " + to_string(connection->getDestinationLink()->getDestinationNode()) + " closed: Freeing " + to_string(connection->getFirstFSUOfInputLink()) + "-" + to_string(connection->getFirstFSUOfInputLink() + connection->getRequiredNumberOfFSUs() - 1) + " in input link, " + to_string(connection->getFirstFSUOfInternalLinks()) + "-" + to_string(connection->getFirstFSUOfInternalLinks() + connection->getRequiredNumberOfFSUs() - 1) + " in internal links, " + to_string(connection->getFirstFSUOfOutputLink()) + "-" + to_string(connection->getFirstFSUOfOutputLink() + connection->getRequiredNumberOfFSUs() - 1) + " in output link (" + to_string(connection->getRequiredNumberOfFSUs()) + " FSUs)");
 	}
+	delete connection;
 }
--- src/event/EventNewCallArrivalErlangClass.cpp
+++ src/event/EventNewCallArrivalErlangClass.cpp
@@ -8,6 +8,10 @@ void EventNewCallArrivalErlangClass::execute(Network &network, priority_queue<Ev
 	Logger::instance().log(occurrenceTime, generator.getA(), generator.getSimulationIndex(), Logger::CONNECTION_SETUP, "Setting up connection from input: " + to_string(connection->getSourceLink()->getSourceNode()) + " to direction " + to_string(connection->getOutputDirection()->at(0)->getDestinationNode()) + "...");
 	trafficClassStatistics.totalNumberOfCalls++;
 
+	eventQueue.push(new EventNewCallArrivalErlangClass(occurrenceTime + generator.getRandomOccurrenceTime(connection->getRequiredNumberOfFSUs(), network.getNumberOfInputLinks()),
+													   new Connection(network.getRandomInputLink(generator), network.getRandomOutputDirection(generator), connection->getRequiredNumberOfFSUs(), generator.getRandomServiceTime()),
+													   trafficClassStatistics));
+
 	switch (network.checkIfConnectionCanBeEstablished(connection, generator)) {
 		case Network::CONNECTION_CAN_BE_ESTABLISHED:
 			trafficClassStatistics.establishedConnections++;
@@ -25,18 +29,17 @@ void EventNewCallArrivalErlangClass::execute(Network &network, priority_queue<Ev
 		case Network::CONNECTION_REJECTED:
 			trafficClassStatistics.totalNumberOfCalls--;
 			Logger::instance().log(occurrenceTime, generator.getA(), generator.getSimulationIndex(), Logger::CONNECTION_REJECTED, "Connection rejected: free FSUs not found in source link (" + to_string(connection->getRequiredNumberOfFSUs()) + " FSUs)");
+			delete connection;
 			break;
 		case Network::INTERNAL_BLOCK:
 			trafficClassStatistics.internalBlocksCount++;
 			Logger::instance().log(occurrenceTime, generator.getA(), generator.getSimulationIndex(), Logger::INTERNAL_BLOCK, "Connection rejected: internal blocking (" + to_string(connection->getRequiredNumberOfFSUs()) + " FSUs)");
+			delete connection;
 			break;
 		case Network::EXTERNAL_BLOCK:
 			trafficClassStatistics.externalBlocksCount++;
 			Logger::instance().log(occurrenceTime, generator.getA(), generator.getSimulationIndex(), Logger::EXTERNAL_BLOCK, "Connection rejected: external blocking (" + to_string(connection->getRequiredNumberOfFSUs()) + " FSUs)");
+			delete connection;
 			break;
 	}
-
-	eventQueue.push(new EventNewCallArrivalErlangClass(occurrenceTime + generator.getRandomOccurrenceTime(connection->getRequiredNumberOfFSUs(), network.getNumberOfInputLinks()),
-													   new Connection(network.getRandomInputLink(generator), network.getRandomOutputDirection(generator), connection->getRequiredNumberOfFSUs(), generator.getRandomServiceTime()),
-													   trafficClassStatistics));
 }
